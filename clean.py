fa628636b70afce7e3b6e748a85df8fc41e09413
ferhatgec <ferhatgectao@gmail.com>
2024-11-10 20:30:17 +0300

    Separate: GLFW from fre2d, fetch and link it through example_project.

--- clean.py
+++ clean.py
@@ -1,11 +1,16 @@
 # use this to remove libs/ artifacts fetched by CMake.
 import shutil
 import os
+import stat
+
+def remove_readonly(func, path, exc_info):
+  os.chmod(path, stat.S_IWRITE)
+  func(path)
 
 for it in [ f.path for f in os.scandir('./libs/') if f.is_dir()]:
   if it == './libs/glad':
     continue
-  shutil.rmtree(it)
+  shutil.rmtree(it, onerror=remove_readonly)
   os.makedirs(it)
   with open(f'{it}/.gitkeep', 'w') as file_stream:
     pass
